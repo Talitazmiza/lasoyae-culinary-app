d582d4d1594f7ca95034c11ca351199f0057968e
Diana Fitri <diana.fitri@uisi.ac.id>
2021-11-09 19:44:07 +0700

    Nambahin komponen buat daftar

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,29 +1,106 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import '../App.css';
+import {connect, useDispatch, useSelector} from "react-redux";
+import {
+    getAllProducts
+} from "../Actions/products";
+import { logout } from "../Actions/auth";
 
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import LogoutIcon from '@mui/icons-material/Logout'
+import {Redirect} from "react-router-dom";
 const Home = () => {
 
-    // some...
+    const products = useSelector(state => state.products);
+    const dispatch = useDispatch();
+    const headers = ['No','Name', 'Stock', 'Price', 'Action']
+    useEffect(() => {
+        dispatch(getAllProducts());
+    }, []);
 
+    const logOut = () => {
+        dispatch(logout())
+    };
     return (
-        <div className="2xl:bg-warning">
-            <div className="navbar mb-2 shadow-lg bg-warning text-neutral-content ">
-                <div className="flex-1 px-2 mx-2">
+        <div>
+            <div className="2xl:bg-warning">
+                <div className="navbar mb-2 shadow-lg bg-warning text-neutral-content ">
+                    <div className="flex-1 px-2 mx-2">
                     <span className="text-lg font-bold">
                         LASOYAE
                     </span>
+                    </div>
+                    <div className="flex-none">
+                        <button className="btn btn-square btn-ghost mr-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                 className="inline-block w-6 h-6 stroke-current">
+                                <ShoppingCartIcon/>
+                            </svg>
+                        </button>
+                        <a href="/login" onClick={logOut} className="btn btn-square btn-ghost">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                 className="inline-block w-6 h-6 stroke-current">
+                                <LogoutIcon/>
+                            </svg>
+                        </a>
+                    </div>
                 </div>
-                <div className="flex-none">
-                    <button className="btn btn-square btn-ghost">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                             className="inline-block w-6 h-6 stroke-current">
-                            <ShoppingCartIcon/>
-                        </svg>
-                    </button>
+            </div>
+            <div className="container mx-auto p-5">
+                <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                    <table className="min-w-full divide-y divide-gray-200">
+                        <thead className="bg-gray-50">
+                            <tr>
+                                {headers.map(column => (
+                                    // Add the sorting props to control sorting. For this example
+                                    // we can add them into the header props
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                    >
+                                        {column}
+                                    </th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody className="bg-white divide-y divide-gray-200">
+                                    {products && products.map((cell, index) => {
+                                        return (
+                                            <tr>
+                                                <td
+                                                    className="px-6 py-4 whitespace-nowrap">
+                                                    {index+1}
+                                                </td>
+                                                <td
+                                                    className="px-6 py-4 whitespace-nowrap">
+                                                    {cell.nama_produk}
+                                                </td>
+                                                <td
+                                                    className="px-6 py-4 whitespace-nowrap">
+                                                    {cell.stock}
+                                                </td>
+                                                <td
+                                                    className="px-6 py-4 whitespace-nowrap">
+                                                    {cell.harga}
+                                                </td>
+                                                <td
+                                                    className="px-6 py-4 whitespace-nowrap">
+                                                    <EditIcon/>
+                                                    <DeleteIcon/>
+                                                </td>
+                                            </tr>
+
+                                        )
+                                    })}
+
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
+
 
     );
 };
---
+++ src/Components/Register.js
@@ -0,0 +1,158 @@
+import '../App.css';
+import React, {useState, useRef} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {Redirect} from 'react-router-dom';
+import {register} from "../Actions/auth";
+
+const Register = () => {
+    const initialState = {
+        name: null,
+        email: null,
+        password: null,
+        tanggal_lahir: null,
+        berat_badan : null,
+        tinggi_badan : null
+    };
+    const [form, setForm] = useState(initialState);
+    const [loading, setLoading] = useState(false);
+
+    const {isLoggedIn} = useSelector(state => state.auth);
+    const {message} = useSelector(state => state.message);
+
+    const dispatch = useDispatch();
+
+    const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+
+        setLoading(true);
+        dispatch(register(form))
+            .then(() => {
+                return <Redirect to="/home" />;
+            })
+            .catch(() => {
+                setLoading(false);
+            });
+
+    };
+    if (isLoggedIn) {
+        return <Redirect to="/home" />;
+    }
+    return (
+        <div className="hero min-h-screen bg-warning">
+            <div className="flex-col justify-center hero-content lg:flex-row ">
+                <div className="card flex-shrink-0 w-full max-w-lg shadow-2xl">
+                    <div className="card-body bg-white">
+                        <form onSubmit={handleRegister}>
+                            <div className="grid grid-cols-2 gap-4">
+                                <div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text text-black">Nama Lengkap</span>
+                                        </label>
+                                        <input
+                                            name="name"
+                                            onChange={handleChange}
+                                            type="text"
+                                            placeholder="Nama lengkap"
+                                            required
+                                            className="input input-bordered border-base-100 bg-white"/>
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text text-black">Email</span>
+                                        </label>
+                                        <input
+                                            name="email"
+                                            onChange={handleChange}
+                                            type="email"
+                                            placeholder="E-Mail"
+                                            required
+                                            className="input input-bordered border-base-100 bg-white"/>
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text text-black">Password</span>
+                                        </label>
+                                        <input
+                                            name="password"
+                                            onChange={handleChange}
+                                            type="password"
+                                            placeholder="Password"
+                                            className="input input-bordered border-base-100 bg-white"/>
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text text-black">Tanggal lahir</span>
+                                        </label>
+                                        <input
+                                            name="tanggal_lahir"
+                                            onChange={handleChange}
+                                            type="date"
+                                            placeholder="Tanggal lahir"
+                                            required
+                                            className="input input-bordered border-base-100 bg-white"/>
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text text-black">Berat badan</span>
+                                        </label>
+                                        <input
+                                            name="berat_badan"
+                                            onChange={handleChange}
+                                            type="number"
+                                            placeholder="Berat badan (Kg)"
+                                            required
+                                            className="input input-bordered border-base-100 bg-white"/>
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text text-black">Password</span>
+                                        </label>
+                                        <input
+                                            name="tinggi_badan"
+                                            onChange={handleChange}
+                                            type="number"
+                                            placeholder="Tinggi badan (cm)"
+                                            className="input input-bordered border-base-100 bg-white"/>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-accent" disabled={loading}>
+                                    {loading && (
+                                        <svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
+                                        </svg>
+                                    )}
+                                    <span>Register</span>
+                                </button>
+                            </div>
+                            {message && (
+                                <div className="form-control mt-6">
+                                    <div className="alert alert-error">
+                                        <div className="flex-1">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                 className="w-6 h-6 mx-2 stroke-current">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                      d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"></path>
+                                            </svg>
+                                            <label>{message}</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
