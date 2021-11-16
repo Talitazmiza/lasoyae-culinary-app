1ec6ebadb77d9fac2eb0ef7910f8656523fca09d
Talita <talitazmiza@gmail.com>
2021-11-16 19:17:26 +0700

    config tangan melambai

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,33 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: [
+      './src/**/*.{js,jsx,ts,tsx}',
+      './public/index.html'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    // colors: {
-    //   'yellow': '#FBBF24',
-    // },
+    extend: {
+      keyframes: {
+        wave: {
+          '0%': { transform: 'rotate(0.0deg)' },
+          '15%': { transform: 'rotate(14.0deg)' },
+          '30%': { transform: 'rotate(-8.0deg)' },
+          '40%': { transform: 'rotate(14.0deg)' },
+          '50%': { transform: 'rotate(-4.0deg)' },
+          '60%': { transform: 'rotate(10.0deg)' },
+          '70%': { transform: 'rotate(0.0deg)' },
+          '100%': { transform: 'rotate(0.0deg)' },
+        }
+      },
+      animation: {
+        wave: 'wave 1.5s infinite'
+      }
+    }
   },
 
   variants: {
-    extend: {},
+    extends: {
+
+    }
   },
 
   plugins: [
