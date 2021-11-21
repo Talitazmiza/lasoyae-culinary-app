f24cbe16d3477dbdd9181d59130f47fe1c0aee90
Diana Fitri <diana.fitri@uisi.ac.id>
2021-11-21 19:12:21 +0700

    refactor landing page dan loading data dari api

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,8 +1,12 @@
 import '../App.css';
 import '../index.css';
 import React from 'react';
+import {useSelector} from "react-redux";
+import {Link} from "react-router-dom";
 
 const Landing = () => {
+    const {isLoggedIn} = useSelector(state => state.auth);
+    const {user} = useSelector(state => state.auth);
 
     // some logics mayb
 
@@ -14,9 +18,26 @@ const Landing = () => {
                     <h1 className="mb-9 text-5xl font-bold">
                         Welcome To Lasoyae
                     </h1>
-                    <a href="/login" className="btn glass">
-                        <p>Continue with Login</p>
-                    </a>
+                    {
+                        isLoggedIn !== true ? (
+                            <Link to="/login" className="btn glass">
+                                <p>Continue with Login</p>
+                            </Link>) :
+                            (<div>
+                                {
+                                    user!= null && user.email == 'admin@gmail.com' ? (
+                                    <Link to="/dashboard" className="btn glass">
+                                            <p>Go to Dashboard</p>
+                                    </Link>) : (
+                                        <Link to="/foods" className="btn glass">
+                                            <p>Let's order!</p>
+                                        </Link>
+                                    )
+                                }
+
+                            </div>)
+                    }
+
                     {/*/!*<button href="/login" className="btn glass">Continue with Login</button>*!/*/}
                 </div>
             </div>
--- src/Views/Admin/Components/Stocks.js
+++ src/Views/Admin/Components/Stocks.js
@@ -73,7 +73,7 @@ export default function Stocks() {
                     })}
 
                         </tbody>
-                        </table>) : <div className="flex justify-center"><div className="animate-spin rounded-full h-20 w-20 border-b-2 border-gray-900"/></div>
+                        </table>) : <div className="flex justify-center"><div className="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-gray-900"/></div>
                     }
                 </div>
             </div>
